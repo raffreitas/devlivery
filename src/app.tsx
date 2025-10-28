21e46fa8a062a7cf5b0801cbb2181cbca2199722
Rafael Freitas <raffreitasx@gmail.com>
2025-10-28 14:09:12 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="bg-gray-900 text-gray-100 h-screen w-screen">
+      <h1 className="text-center text-2xl">Devlivery WebApp</h1>
+    </div>
+  );
+}
