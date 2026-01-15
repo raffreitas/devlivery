43d8b23d3d32eee3af12696928b91fa53ac60d6d
Rafael Freitas <raffreitasx@gmail.com>
2026-01-15 20:26:35 -0300

    fix: update database name for devlivery configuration

--- src/Devlivery.AppHost/AppHost.cs
+++ src/Devlivery.AppHost/AppHost.cs
@@ -5,7 +5,7 @@ var postgres = builder.AddPostgres("postgres")
     .WithHostPort(5432)
     .WithLifetime(ContainerLifetime.Persistent);
 
-var devliveryDb = postgres.AddDatabase("devlivery-db", "devlivery_restored");
+var devliveryDb = postgres.AddDatabase("devlivery-db", "devlivery");
 
 builder.AddProject<Projects.Devlivery>("devlivery-webapi")
     .WaitFor(devliveryDb)
