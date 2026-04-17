1bf9b6a5bf4827ed437f5eba33c00c7baf712095
Rafael Freitas <raffreitasx@gmail.com>
2026-04-17 13:23:00 -0300

    feat: add DbContextFactory classes for application and identity contexts

---
+++ apps/api/src/Devlivery/Infrastructure/Identity/Context/ApplicationIdentityDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Devlivery.Common.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Devlivery.Infrastructure.Identity.Context;
+
+internal sealed class ApplicationIdentityDbContextFactory : IDesignTimeDbContextFactory<ApplicationIdentityDbContext>
+{
+    public ApplicationIdentityDbContext CreateDbContext(string[] args)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connectionString = configuration.GetConnectionStringOrThrow("DatabaseConnection");
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationIdentityDbContext>();
+        optionsBuilder.UseNpgsql(connectionString, sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure();
+        }).UseSnakeCaseNamingConvention();
+        return new ApplicationIdentityDbContext(optionsBuilder.Options);
+    }
+}
---
+++ apps/api/src/Devlivery/Infrastructure/Persistence/Factory/ApplicationDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Devlivery.Common.Extensions;
+using Devlivery.Infrastructure.Persistence.Context;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Devlivery.Infrastructure.Persistence.Factory;
+
+internal sealed class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile($"appsettings.{environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connectionString = configuration.GetConnectionStringOrThrow("DatabaseConnection");
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(connectionString, sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure();
+        }).UseSnakeCaseNamingConvention();
+        return new ApplicationDbContext(optionsBuilder.Options, null!);
+    }
+}
