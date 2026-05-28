82af74e93a5a03468fdd372be4549eb24bc044a4
Rafael Freitas <raffreitasx@gmail.com>
2026-05-28 19:59:02 -0300

    chore: update package references to latest versions and change BackupOptions to record type

--- apps/api/src/Devlivery.BackupJob/Configuration/BackupOptions.cs
+++ apps/api/src/Devlivery.BackupJob/Configuration/BackupOptions.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Devlivery.BackupJob.Configuration;
 
-public sealed class BackupOptions
+public sealed record BackupOptions
 {
     public const string SectionName = "Backup";
 
